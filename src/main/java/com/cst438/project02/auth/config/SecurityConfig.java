749dfbd1e64a749c8e9db39efe7035618d89a8a7
Sergio Zavala <sezavala@csumb.edu>
2025-10-21 18:39:00 -0700

    h2

--- src/main/java/com/cst438/project02/auth/config/SecurityConfig.java
+++ src/main/java/com/cst438/project02/auth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cst438.project02.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,10 +16,11 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // Filter Chain to allow anyone to access to the Google Auth route.
-        return http
+        return (SecurityFilterChain) http
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/v1/auth/**").permitAll();
                     auth.requestMatchers("/error").permitAll();
+                    auth.requestMatchers("/h2-console/**").permitAll();
                     auth.requestMatchers("https://developers.google.com/oauthplayground/*").permitAll();
                     auth.anyRequest().authenticated();
                 })
@@ -26,6 +28,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 // Stateless so we can handle more traffic and not slow down
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(headers -> headers
+                        .frameOptions(Customizer.withDefaults()).disable()
+                )
                 .build();
     }
 
