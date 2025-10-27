47c6076bfbebf2cb0a26576e01e102a4d04bb7a6
emilygrimaldo <167383545+emilygrimaldo@users.noreply.github.com>
2025-10-27 15:41:03 -0700

    Update User.java

--- src/main/java/com/cst438/project02/entity/User.java
+++ src/main/java/com/cst438/project02/entity/User.java
@@ -1,5 +1,4 @@
-package com.cst438.project02.entity;
-
+like this package com.cst438.project02.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,86 +6,65 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = true, unique = true)
-    private String username;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
-    @Column(nullable = true)
-    private String password;
 
-    @Column(name = "password_hash", nullable = true, length = 100)
+@Column(nullable = false, unique = true)
+private String username;
+
+
+@Column(nullable = false)
+private String password;
+
+
+public User() {}
+
+
+public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+}
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public String getUsername() {
+    return username;
+}
+public void setUsername(String username) {
+    this.username = username;
+}
+public String getPassword() {
+    return password;
+}
+public void setPassword(String password) {
+    this.password = password;
+}
+
+}
+
+    // Map to existing column 'password' (nullable for OAuth-only users)
+    @Column(name = "password", nullable = true, length = 100)
     private String passwordHash;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String email;
 
     private String name;
-
-    public User() {}
-
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public User(String email, String name) {
-        this.email = email;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
