2021490cddab85a0052b055376c3249c25f81a1c
Sergio Zavala <sezavala@csumb.edu>
2025-10-27 23:23:27 -0700

    new endpoints

--- src/main/java/com/cst438/project02/entity/Game.java
+++ src/main/java/com/cst438/project02/entity/Game.java
@@ -1,6 +1,7 @@
 package com.cst438.project02.entity;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
- 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,26 +11,38 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 @Entity
 @Table(name = "games")
-
+@Getter
+@Setter
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne //too use from team entity
+    @ManyToOne
     @JoinColumn(name = "home_team_id", nullable = false)
     private Team homeTeam;
-    @ManyToOne //too use from team entity
+
+    @ManyToOne
     @JoinColumn(name = "away_team_id", nullable = false)
     private Team awayTeam;
+
     @Column(name = "game_time", nullable = false)
     private LocalDateTime gameTime;
+
+    // ADD THIS
+    @Column(name = "game_date", nullable = false)
+    private LocalDate gameDate;
+
     @Column(nullable = false)
     private String result;
+
     @OneToMany(mappedBy = "game")
     private List<Bet> bets;
 
@@ -39,45 +52,9 @@ public class Game {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.gameTime = gameTime;
+        this.gameDate = gameTime.toLocalDate(); // Extract date from gameTime
         this.result = result;
     }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Team gethomeTeam() {
-        return homeTeam;
-    }
-    public void sethomeTeam(Team homeTeam) {
-        this.homeTeam = homeTeam;
-    }
-    public Team getawayTeam() {
-        return awayTeam;
-    }
-    public void setawayTeam(Team awayTeam) {
-        this.awayTeam = awayTeam;
-    }
-    public LocalDateTime getgameTime() {
-        return gameTime;
-    }
-    public void setgameTime(LocalDateTime gameTime) {
-        this.gameTime = gameTime;
-    }
-    public String getresult() {
-        return result;
-    }
-    public void setresult(String result) {
-        this.result = result;
-    }
-    public List<Bet> getBets() {
-        return bets;
-    }
-    public void setBets(List<Bet> bets) {
-        this.bets = bets;
-    }
-
 
 }
 
